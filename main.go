31e36fbfe9d5bedd3ff78a245ee831310cc3904c
USER WisZy <pythondevforuser@gmail.com>
2026-04-18 23:03:52 +0545

    project is working

--- main.go
+++ main.go
@@ -1,8 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
-func main (){
-	fmt.Println("Hello world")
+func main() {
+
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://amazon.com",
+		"http://this-site-should-fail.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+	
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
 }
 
+func checkLink(link string, c chan string) {
+
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	_, err := client.Get(link)
+	if err != nil {
+		c <- "❌ " + link + " might be down! Error: " + err.Error()
+		return
+	}
+
+	c <- "✅ " + link + " is up and running!"
+}
